4d91488880c638f09e407194017ca1f6661dba7f
Roy Crombleholme <roy.crombleholme@pam-it.com>
2016-10-11 14:04:33 +0100

    Add generated cookbook content

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'custom_app'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures custom_app'
+long_description 'Installs/Configures custom_app'
+version '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: custom_app
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
